4a052a1586d5681ab0744224ce7badb0f7646b66
ushasree508 <ushasreeorsu5@gmail.com>
2026-07-04 09:39:24 +0530

    Change DB_USER and DB_PASS in InternshipServlet
    Updated database credentials for the InternshipServlet.

--- SkillEdu-main/src/main/java/skill/InternshipServlet.java
+++ SkillEdu-main/src/main/java/skill/InternshipServlet.java
@@ -14,8 +14,8 @@ public class InternshipServlet extends HttpServlet {
 
     // Oracle DB connection details
     private static final String DB_URL = "jdbc:oracle:thin:@localhost:1521:XE";
-    private static final String DB_USER = "sasi";
-    private static final String DB_PASS = "sasi123";
+    private static final String DB_USER = "usha";
+    private static final String DB_PASS = "usha123";
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
